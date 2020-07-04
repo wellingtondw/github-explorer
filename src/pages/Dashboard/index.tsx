707ba667a7ac0d04deea4e21c84d1c9743e10295
Wellington <wellingtonslpbi@hotmail.com>
2020-07-04 16:09:01 -0300

    creating global styles file

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
 
-const Dashboard: React.FC = () => <h1>Dashboard</h1>;
+const Dashboard: React.FC = () => <h1>Explorerepositórios no Github</h1>;
 
 export default Dashboard;
